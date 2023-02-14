bec00f302556823b1288177ec9144e5322a44393
Aleesha <aleeshay@maveric-systems.com>
2023-02-14 22:23:47 +0530

    updated properities file

--- src/main/java/com/maveric/transactionservicetest/dto/BalanceDto.java
+++ src/main/java/com/maveric/transactionservicetest/dto/BalanceDto.java
@@ -3,11 +3,15 @@ package com.maveric.transactionservicetest.dto;
 import com.maveric.transactionservicetest.constants.Currency;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BalanceDto {
     private String  _id;
 
